4ff217b3c23e514222bc668d53c48b775d6b9d1e
BardRoll <thotsapol.nine@gmail.com>
2024-01-28 19:50:09 +0700

    Change 2 decimal places to multiple decimal places

--- show.py
+++ show.py
@@ -28,8 +28,8 @@ def main(data_from_django):
                 check = show.show_csv(load)
                 res = show.check_button_press_sequence(check)
                 len_res = len(res[0])
-                for i in range(0, len_res):
-                    res[0][i] = round(res[0][i], 2)
+                # for i in range(0, len_res):
+                #     res[0][i] = round(res[0][i], 2)
                 time_use = round(res[0][len_res-1] - res[0][0], 2)
                 key = len(check)
                 if res[len(res)-2] == "wrong":
@@ -55,8 +55,8 @@ def main(data_from_django):
                 check = show.show_sequence(key,row,column,color)
                 res = show.check_button_press_sequence(check)
                 len_res = len(res[0])
-                for i in range(0, len_res):
-                    res[0][i] = round(res[0][i], 2)
+                # for i in range(0, len_res):
+                #     res[0][i] = round(res[0][i], 2)
                 time_use = round(res[0][len_res-1] - res[0][0], 2)
                 if res[len(res)-2] == "wrong":
                     result_trial = [key, check, trial+1, res[0], time_use, res[len(res)-1]]
